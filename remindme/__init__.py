d229c066e4022e6500959cd0afc3a1001b8018cc
Vanderlaus <vanderluislauschner@gmail.com>
2023-01-21 15:41:52 -0300

    atualizando banco de dados, backend

--- remindme/__init__.py
+++ remindme/__init__.py
@@ -6,8 +6,20 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.app_context().push()
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
 app.config["SECRET_KEY"] = "dfbb233fe4b817c1070d47c9"
+
+#string conexao
+app.config['SQLALCHEMY_DATABASE_URI'] = '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
+    SGBD = 'postgresql',
+    usuario = "remind",
+    senha = "123456",
+    servidor = "localhost:5435",
+    database = "postgres"
+)
+#app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-from remindme import app
+from remindme import app, db
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
