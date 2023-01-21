98a8743b601c75b42f5e114292a3b3d2b3d4864b
Joao Victor De Oliveira <joao.oliveira@proway.treina>
2023-01-20 21:09:41 -0300

    flask view

--- remindme/views.py
+++ remindme/views.py
@@ -11,38 +11,8 @@ def home_page():
     return render_template("home.html")
 
 @app.route("/tasks", methods=["GET", "POST"])
-@login_required
 def tasks_page():
-    selling_form = SellItemForm()
-    purchase_form = PurchaseItemForm()
-    if request.method == "POST":
-        #Purchase Item
-        purchased_item = request.form.get("purchased_item")
-        p_item_object = Item.query.filter_by(name=purchased_item).first()
-        if p_item_object:
-            if current_user.can_purchase(p_item_object):
-                p_item_object.buy(current_user)
-                flash(f"You purchased {p_item_object.name} for {p_item_object.price}$", category="success")
-            else:
-                flash("Sorry, you don't have enough money to purchase this item...", category="info")
-        #Sell Item
-        sold_item = request.form.get("sold_item")
-        s_item_object = Item.query.filter_by(name=sold_item).first()
-        if s_item_object:
-            if current_user.can_sell(s_item_object):
-                s_item_object.sell(current_user)
-                flash(f"You sold {s_item_object.name} for {s_item_object.price}$", category="success")
-            else:
-                flash(f"Something went wrong with selling {s_item_object.name}", category="danger")
-    
-        return redirect(url_for("market_page"))
-
-
-
-    if request.method == "GET":
-        items = Item.query.filter_by(owner=None)
-        owned_items = Item.query.filter_by(owner=current_user.id)
-        return render_template("market.html", items = items, purchase_form=purchase_form, owned_items = owned_items, selling_form = selling_form)
+    return render_template("tasks.html")
 
 @app.route("/register", methods=["GET", "POST"])
 def register_page():
